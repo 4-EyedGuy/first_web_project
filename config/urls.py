82f943e7725aa8299c63b67011bb6a74012332a3
Alexander Zhgarev <jgarevalexandr@yandex.ru>
2026-04-20 05:32:34 +0300

    Task 8 ready

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import index, about, plugin_detail
+from pages.views import index, about, plugin_detail, contact
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('about/', about, name='about'),
-
+    path('contact/', contact, name='contact'),
     path('plugin/<int:pk>/', plugin_detail, name='plugin_detail'),
 ]
 
